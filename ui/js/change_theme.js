7708402c38c4ff43e179d9eeefa1f29fb9d3218f
symlPigeon <2163953074@qq.com>
2024-05-04 15:23:27 +0800

    Added 4 themes. The desktop file has been improved. Since the save configuration function has not yet been implemented, optional themes need to be reconfigured every time they are started. These contents are expected to be added in the next update.

--- ui/js/change_theme.js
+++ ui/js/change_theme.js
@@ -1,16 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
     const { listen } = window.__TAURI__.event;
 
+    // Initial theme...
+    lyrics = document.getElementById("lyric");
+    lyrics.className = "lyric light-blue";
+
     listen("menu_set_theme", (e) => {
         context = e.payload;
+        lyrics = document.getElementById("lyric");
         switch (context) {
             case "light_blue":
+                lyrics.className = "lyric light-blue";
+                console.log("changing to light blue");
                 break;
             case "green":
+                lyrics.className = "lyric green";
                 break;
             case "yellow":
+                lyrics.className = "lyric yellow";
                 break;
             case "red":
+                lyrics.className = "lyric red";
                 break;
             default:
                 break;
